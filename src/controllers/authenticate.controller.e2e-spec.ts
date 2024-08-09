918f869d502fa9b511840c32d239d8705cc62195
OJailson17 <JaylsonO17@gmail.com>
2024-08-09 05:57:55 -0300

    test: add e2e tests to authenticate controller

---
+++ src/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,50 @@
+import { AppModule } from '@/app.module';
+import { PrismaService } from '@/prisma/prisma.service';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { hash } from 'bcryptjs';
+import request from 'supertest';
+import { string } from 'zod';
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[POST] /sessions', async () => {
+    const user = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    };
+
+    await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        password: await hash(user.password, 8),
+      },
+    });
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual({
+      access_token: expect.any(String),
+    });
+  });
+});
