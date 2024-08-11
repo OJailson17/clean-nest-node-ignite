2b8da7a2fde71f81216fe45e3caeedb92728343b
OJailson17 <JaylsonO17@gmail.com>
2024-08-11 09:31:40 -0300

    chore: implement findById method on prisma repository

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -2,9 +2,13 @@ import { PaginationParams } from '@/core/repositories/pagination-params';
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
 import { Question } from '@/domain/forum/enterprise/entities/question';
 import { Injectable } from '@nestjs/common';
+import { PrismaQuestionMapper } from '../../mappers/prisma-question-mapper';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(question: Question): Promise<void> {
     throw new Error('Method not implemented.');
   }
@@ -13,8 +17,16 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     throw new Error('Method not implemented.');
   }
 
-  findById(questionId: string): Promise<Question | null> {
-    throw new Error('Method not implemented.');
+  async findById(questionId: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id: questionId,
+      },
+    });
+
+    if (!question) return null;
+
+    return PrismaQuestionMapper.toDomain(question);
   }
 
   findBySlug(slug: string): Promise<Question | null> {
