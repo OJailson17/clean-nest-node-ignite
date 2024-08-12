ce5eb5f9d9aacd8419201519636cdc7db192ff67
OJailson17 <JaylsonO17@gmail.com>
2024-08-12 09:37:19 -0300

    chore: use repository on the fetch recent questions

--- src/domain/forum/application/use-cases/fetch-recent-questions-use-case.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions-use-case.ts
@@ -1,28 +1,30 @@
 import { Either, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
 import { Question } from '../../enterprise/entities/question';
 import { QuestionsRepository } from '../repositories/questions-repository';
 
 interface FetchRecentQuestionsRequest {
-	page: number;
+  page: number;
 }
 
 type FetchRecentQuestionsResponse = Either<
-	null,
-	{
-		questions: Question[];
-	}
+  null,
+  {
+    questions: Question[];
+  }
 >;
 
+@Injectable()
 export class FetchRecentQuestionsUseCase {
-	constructor(private questionsRepository: QuestionsRepository) {}
+  constructor(private questionsRepository: QuestionsRepository) {}
 
-	async execute({
-		page,
-	}: FetchRecentQuestionsRequest): Promise<FetchRecentQuestionsResponse> {
-		const questions = await this.questionsRepository.findManyRecent({ page });
+  async execute({
+    page,
+  }: FetchRecentQuestionsRequest): Promise<FetchRecentQuestionsResponse> {
+    const questions = await this.questionsRepository.findManyRecent({ page });
 
-		return right({
-			questions,
-		});
-	}
+    return right({
+      questions,
+    });
+  }
 }
