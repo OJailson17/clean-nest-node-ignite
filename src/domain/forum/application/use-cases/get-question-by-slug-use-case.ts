903e3c2d92a84c7a317c9b9ab8a1927129f30e23
OJailson17 <JaylsonO17@gmail.com>
2024-08-22 23:04:06 -0300

    test: add e2e tests to get question by slug controller

--- src/domain/forum/application/use-cases/get-question-by-slug-use-case.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug-use-case.ts
@@ -2,32 +2,34 @@ import { Either, left, right } from '@/core/either';
 import { Question } from '../../enterprise/entities/question';
 import { QuestionsRepository } from '../repositories/questions-repository';
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
+import { Injectable } from '@nestjs/common/decorators';
 
 interface GetQuestionBySlugRequest {
-	slug: string;
+  slug: string;
 }
 
 type GetQuestionBySlugResponse = Either<
-	ResourceNotFoundError,
-	{
-		question: Question;
-	}
+  ResourceNotFoundError,
+  {
+    question: Question;
+  }
 >;
 
+@Injectable()
 export class GetQuestionBySlugUseCase {
-	constructor(private questionsRepository: QuestionsRepository) {}
+  constructor(private questionsRepository: QuestionsRepository) {}
 
-	async execute({
-		slug,
-	}: GetQuestionBySlugRequest): Promise<GetQuestionBySlugResponse> {
-		const question = await this.questionsRepository.findBySlug(slug);
+  async execute({
+    slug,
+  }: GetQuestionBySlugRequest): Promise<GetQuestionBySlugResponse> {
+    const question = await this.questionsRepository.findBySlug(slug);
 
-		if (!question) {
-			return left(new ResourceNotFoundError());
-		}
+    if (!question) {
+      return left(new ResourceNotFoundError());
+    }
 
-		return right({
-			question,
-		});
-	}
+    return right({
+      question,
+    });
+  }
 }
