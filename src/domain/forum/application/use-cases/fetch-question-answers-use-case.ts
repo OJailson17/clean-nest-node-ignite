0056858e65f86a19f9363e2697a18a5988d4f431
OJailson17 <JaylsonO17@gmail.com>
2024-08-26 01:08:27 -0300

    feat: add fetch question answers controller

--- src/domain/forum/application/use-cases/fetch-question-answers-use-case.ts
+++ src/domain/forum/application/use-cases/fetch-question-answers-use-case.ts
@@ -1,33 +1,35 @@
 import { Either, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
 import { Answer } from '../../enterprise/entities/answer';
 import { AnswersRepository } from '../repositories/answers-repository';
 
 interface FetchQuestionAnswersRequest {
-	page: number;
-	questionId: string;
+  page: number;
+  questionId: string;
 }
 
 type FetchQuestionAnswersResponse = Either<
-	null,
-	{
-		answers: Answer[];
-	}
+  null,
+  {
+    answers: Answer[];
+  }
 >;
 
+@Injectable()
 export class FetchQuestionAnswersUseCase {
-	constructor(private answersRepository: AnswersRepository) {}
+  constructor(private answersRepository: AnswersRepository) {}
 
-	async execute({
-		questionId,
-		page,
-	}: FetchQuestionAnswersRequest): Promise<FetchQuestionAnswersResponse> {
-		const answers = await this.answersRepository.findManyByQuestionId(
-			questionId,
-			{ page },
-		);
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersRequest): Promise<FetchQuestionAnswersResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page },
+    );
 
-		return right({
-			answers,
-		});
-	}
+    return right({
+      answers,
+    });
+  }
 }
