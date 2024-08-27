786b62ad3b848e68b61e8c16166b0019c0f621de
OJailson17 <JaylsonO17@gmail.com>
2024-08-26 23:36:45 -0300

    feat: add delete question comment controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,6 +4,7 @@ import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-
 import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question-use-case';
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question-use-case';
 import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer-use-case';
+import { DeleteQuestionCommentUseCase } from '@/domain/forum/application/use-cases/delete-question-comment-use-case';
 import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question-use-case';
 import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer-use-case';
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question-use-case';
@@ -21,6 +22,7 @@ import { CommentOnQuestionController } from './controllers/comment-on-question.c
 import { CreateAccountController } from './controllers/create-account.controller';
 import { CreateQuestionController } from './controllers/create-question.controller';
 import { DeleteAnswerController } from './controllers/delete-answer.controller';
+import { DeleteQuestionCommentController } from './controllers/delete-question-comment.controller';
 import { DeleteQuestionController } from './controllers/delete-question.controller';
 import { EditAnswerController } from './controllers/edit-answer.controller';
 import { EditQuestionController } from './controllers/edit-question.controller';
@@ -44,6 +46,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     FetchQuestionAnswersController,
     ChooseQuestionBestAnswerController,
     CommentOnQuestionController,
+    DeleteQuestionCommentController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -59,6 +62,7 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
     FetchQuestionAnswersUseCase,
     ChooseQuestionBestAnswerUseCase,
     CommentOnQuestionUseCase,
+    DeleteQuestionCommentUseCase,
   ],
 })
 export class HttpModule {}
