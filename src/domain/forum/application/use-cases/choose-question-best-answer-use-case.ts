bff0858537e7abd988608e8de51d51d3755c48af
OJailson17 <JaylsonO17@gmail.com>
2024-08-26 22:30:47 -0300

    feat: add choose question best answer controller

--- src/domain/forum/application/use-cases/choose-question-best-answer-use-case.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer-use-case.ts
@@ -4,53 +4,55 @@ import { QuestionsRepository } from '../repositories/questions-repository';
 import { Either, left, right } from '@/core/either';
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
 import { NotAllowedError } from '@/core/errors/not-allowed-error';
+import { Injectable } from '@nestjs/common';
 
 interface ChooseQuestionBestAnswerUseCaseRequest {
-	answerId: string;
-	authorId: string;
+  answerId: string;
+  authorId: string;
 }
 
 type ChooseQuestionBestAnswerUseCaseResponse = Either<
-	ResourceNotFoundError | NotAllowedError,
-	{
-		question: Question;
-	}
+  ResourceNotFoundError | NotAllowedError,
+  {
+    question: Question;
+  }
 >;
 
+@Injectable()
 export class ChooseQuestionBestAnswerUseCase {
-	constructor(
-		private questionsRepository: QuestionsRepository,
-		private answersRepository: AnswersRepository,
-	) {}
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private answersRepository: AnswersRepository,
+  ) {}
 
-	async execute({
-		answerId,
-		authorId,
-	}: ChooseQuestionBestAnswerUseCaseRequest): Promise<ChooseQuestionBestAnswerUseCaseResponse> {
-		const answer = await this.answersRepository.findById(answerId);
+  async execute({
+    answerId,
+    authorId,
+  }: ChooseQuestionBestAnswerUseCaseRequest): Promise<ChooseQuestionBestAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId);
 
-		if (!answer) {
-			return left(new ResourceNotFoundError());
-		}
+    if (!answer) {
+      return left(new ResourceNotFoundError());
+    }
 
-		const question = await this.questionsRepository.findById(
-			answer.questionId.toString(),
-		);
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString(),
+    );
 
-		if (!question) {
-			return left(new ResourceNotFoundError());
-		}
+    if (!question) {
+      return left(new ResourceNotFoundError());
+    }
 
-		if (authorId !== question.authorId.toString()) {
-			return left(new NotAllowedError());
-		}
+    if (authorId !== question.authorId.toString()) {
+      return left(new NotAllowedError());
+    }
 
-		question.bestAnswerId = answer.id;
+    question.bestAnswerId = answer.id;
 
-		await this.questionsRepository.save(question);
+    await this.questionsRepository.save(question);
 
-		return right({
-			question,
-		});
-	}
+    return right({
+      question,
+    });
+  }
 }
